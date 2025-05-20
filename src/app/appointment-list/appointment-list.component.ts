36fc7a266e4d6ad4998ca371017cc8b9f41376f1
pheyboer <heyboer.peter@gmail.com>
2025-05-20 15:07:54 -0400

    adding appointments to local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -25,10 +25,12 @@ export class AppointmentListComponent {
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
 
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
     }
   }
 
   deleteAppointment(index: number) {
     this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
 }
