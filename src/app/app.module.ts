f66e941f5f233ec7f4f45b9c3df2914ff617b0af
pheyboer <heyboer.peter@gmail.com>
2025-05-20 14:39:08 -0400

    adding appointments to the array with alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppComponent } from './app.component';
 import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AppointmentListComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, AppointmentListComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -4,17 +4,28 @@ import { Appointment } from '../models/appointment';
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+  styleUrls: ['./appointment-list.component.css'],
 })
 export class AppointmentListComponent {
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
 
-  newAppointmentTitle : string = "";
-  newAppointmentDate : Date = new Date();
+  appointments: Appointment[] = [];
 
-  appointments: Appointment[] = []
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
 
-  addAppointment(){
-    alert(this.newAppointmentTitle + " " + this.newAppointmentDate)
+      this.appointments.push(newAppointment);
+
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+
+      alert(this.appointments.length);
+    }
   }
-  
 }
